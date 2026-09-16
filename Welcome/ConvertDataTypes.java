3259f4c1982b490bd6ee6d930cc01b6bcb50822e
ashila-AS <ashila.aulia.tif25@polban.ac.id>
2026-09-16 08:41:40 +0700

    Reformat ConvertDataTypes.java for consistency

--- Welcome/ConvertDataTypes.java
+++ Welcome/ConvertDataTypes.java
@@ -9,8 +9,8 @@ public class ConvertDataTypes
     public static void main(String[] args) 
     {
         double d = 10.25;
-        float f = (float) d;               // f = 10.25f
-        byte b = (byte) methodOne((long) f); // (long) f = 10 -> methodOne(10) -> 10 -> b = 10
-        System.out.println(b); // output: 10
+        float f = (float) d;               
+        byte b = (byte) methodOne((long) f); 
+        System.out.println(b); 
     }
 }
